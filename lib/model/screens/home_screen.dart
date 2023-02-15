3e987b8354637caa3f6fc29540240c872de3185d
Wiksem <clement.redoules@hotmail.com>
2023-02-15 09:44:29 +0100

    sauvegarde

--- lib/model/screens/home_screen.dart
+++ lib/model/screens/home_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import '../constantes.dart';
 import '../question_model.dart';
 import '../widgets/question_widget.dart';
 import '../widgets/next_button.dart';
 import '../widgets/option_card.dart';
+import '../widgets/result_box.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,21 +17,24 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Question> _questions = [
-    Question(
-      id: '10',
-      title: 'question1',
-      options: {'faux': false, 'faux': false, 'faux': false, 'vrai': true},
-    ),
-    Question(
-      id: '11',
-      title: 'question2',
-      options: {'faux': false, 'faux': false, 'faux': false, 'vrai': true},
-    ),
-    Question(
-      id: '12',
-      title: 'question3',
-      options: {'faux': false, 'faux': false, 'faux': false, 'vrai': true},
-    ),
+    Question(id: '10', title: 'question1', options: {
+      'faux': false,
+      'vrai': true,
+      'faux': false,
+      'faux': false,
+    }),
+    Question(id: '11', title: 'question2', options: {
+      'faux': false,
+      'vrai': true,
+      'faux': false,
+      'faux': false,
+    }),
+    Question(id: '12', title: 'question3', options: {
+      'faux': false,
+      'vrai': true,
+      'faux': false,
+      'faux': false,
+    }),
   ];
 
   int index = 0;
@@ -37,14 +43,23 @@ class _HomeScreenState extends State<HomeScreen> {
 
   bool isPressed = false;
 
+  bool isAlreadySelected = false;
+
   void nextQuestion() {
     if (index == _questions.length - 1) {
+      showDialog(
+          context: context,
+          builder: (ctx) => ResultBox(
+                result: score,
+                questionLength: _questions.length,
+              ));
       return;
     } else {
       if (isPressed) {
         setState(() {
           index++;
           isPressed = false;
+          isAlreadySelected = false;
         });
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
@@ -58,10 +73,18 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  void changeColor() {
-    setState(() {
-      isPressed = true;
-    });
+  void checkAnswerAndUpdate(bool value) {
+    if (isAlreadySelected) {
+      return;
+    } else {
+      if (value == true) {
+        score++;
+        setState(() {
+          isPressed = true;
+          isAlreadySelected = true;
+        });
+      }
+    }
   }
 
   @override
@@ -78,7 +101,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Text(
                 'Score: $score',
                 style: const TextStyle(fontSize: 18.0),
-                ),
+              ),
             ),
           ]),
       body: Container(
@@ -94,14 +117,17 @@ class _HomeScreenState extends State<HomeScreen> {
             const Divider(color: neutral),
             const SizedBox(height: 25.0),
             for (int i = 0; i < _questions[index].options.length; i++)
-              OptionCard(
-                option: _questions[index].options.keys.toList()[i],
-                color: isPressed
-                    ? _questions[index].options.values.toList()[i] == true
-                        ? correct
-                        : incorrect
-                    : neutral,
-                onTap: changeColor,
+              GestureDetector(
+                onTap: () => checkAnswerAndUpdate(
+                    _questions[index].options.values.toList()[i]),
+                child: OptionCard(
+                  option: _questions[index].options.keys.toList()[i],
+                  color: isPressed
+                      ? _questions[index].options.values.toList()[i] == true
+                          ? correct
+                          : incorrect
+                      : neutral,
+                ),
               ),
           ],
         ),
--- lib/model/widgets/option_card.dart
+++ lib/model/widgets/option_card.dart
@@ -2,18 +2,16 @@ import 'package:flutter/material.dart';
 import '../constantes.dart';
 
 class OptionCard extends StatelessWidget {
-  const OptionCard({Key? key, required this.option, 
+  const OptionCard({
+  Key? key, 
+  required this.option, 
   required this.color, 
-  required this.onTap})
-      : super(key: key);
-  final String option;
-  final VoidCallback onTap; 
+  }) : super(key: key);
+  final String option; 
   final Color color;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Card(
+    return Card(
         color: color,
         child: ListTile(
           title: Text(
@@ -21,10 +19,10 @@ class OptionCard extends StatelessWidget {
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 22.0,
+              color: color.red != color.green ? neutral : Colors.black,
             ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
